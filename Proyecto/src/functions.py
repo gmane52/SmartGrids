c543e12b97f2a3cd8f468145ec080cc6a6c93560
gmane <gmane.fr@gmail.com>
2025-12-02 07:00:41 +0100

    - he generado la estructura para trabajar con un ipynb. He añadido todo lo hecho en el punto1, solo falta estudio final y costes. - he empezado la parte2, he generado una función para evaluar que se cumple el criterio N-1 en todos los nodos, y si no se cumple indica si es por sobre/sub tension o cargabilidad de la linea.

--- Proyecto/src/functions.py
+++ Proyecto/src/functions.py
@@ -1,1 +1,46 @@
-# Comment
+# This file have extra functions needed in the main 
+
+import pandapower as pp
+import pandas as pd
+
+def test():
+    return("hello test function")
+
+def evaluar_N_menos1(net, vmin=0.9, vmax=1.1, max_load=80):
+    results = []
+
+    for elementos in net.line.index:
+        net.line.at[elementos, "in_service"] = False # desactivar linea
+
+        # Run simulation
+        pp.runpp(net)
+        success = net["converged"]
+
+        if success:
+            # Buses con tensión fuera de rango
+            V_violated_buses = net.res_bus.index[
+                ~net.res_bus.vm_pu.between(vmin, vmax)
+            ].tolist()
+
+            # Líneas sobrecargadas
+            P_violated_lines = net.res_line.index[
+                net.res_line.loading_percent > max_load
+            ].tolist()
+        else:
+            print(f"Simulation did not converge for line {elementos}")
+            V_violated_buses = []
+            P_violated_lines = []
+
+        passes = success and not V_violated_buses and not P_violated_lines
+
+        results.append({
+            "line": elementos,
+            "success": success,
+            "passes_N-1": passes,
+            "V_violated_buses": V_violated_buses,
+            "P_violated_lines": P_violated_lines
+        })
+
+        net.line.at[elementos, "in_service"] = True #Activar linea
+
+    return pd.DataFrame(results)
